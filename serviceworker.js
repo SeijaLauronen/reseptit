ec14bc2298fff9657d58be28ffdee91806708546
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-07-05 10:55:50 +0300

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,58 +1,84 @@
-const CACHE_NAME = "reseptit-v1";
-const urlsTocache =['index.html'];
+const programVersion = '2024-07-05: 1';
+const staticCacheName = 'recipe-static-1'; 
+const dynamicCacheName = 'recipe-dynamic-1'; // Ei välttämätön
 
-const self = this;
+const assets = [
+  '/',
+  'index.html',
+  'fallback.html'
+];
 
-//Install SW
-self.addEventListener('install',(event)=>{
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsTocache);
-            })
-            .catch((error) => {
-                console.error('Failed to cache', error);
-            })
-    )
 
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('installed,caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
-// Listen for requests
-// TODO jos haluaa index sivun toimivan offline, niin pitää hakea se cachesta??
-self.addEventListener('fetch',(event)=>{
-    event.respondWith(
-        caches.match(event.request)
-            .then(()=>{
-                console.log('Fetch');
-                return fetch(event.request)                
-                    //.catch(()=>caches.match('offline.html'));
-                    .catch(()=>{
-                        console.log('Offline');
-                        caches.match('index.html')
-                    });
-            })
-            .catch((error) => {
-                console.error('Failed to fetch', error);
-            })
-    )
-    
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
-// Activate the SW
-self.addEventListener('activate',(event)=>{
-    const cacheWhitelist = [];
-    cacheWhitelist.push(CACHE_NAME);
-    event.waitUntil(
-        caches.keys().then((cacheNames)=>Promise.all(
-            cacheNames.map((cacheName)=>{
-                if(cacheWhitelist.includes(cacheName)) {
-                    return caches.delete(cacheName);
-                }
-            })
-        ))
-    );
-    
+
+// fetch event
+//https://www.youtube.com/watch?v=ChXgikdQJR8&list=PL4cUxeGkcC9gTxqJBcDmoi5Q2pzDusSL7&index=18
+//responsea ei voi käyttää/ottaa kiinni useampaan kertaan, siksi otetaan siitä kopio cachetta varten!
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          if (fetchRes.clone().status == 404) { //oma iffi
+              if(evt.request.url.indexOf('.html') > -1) {
+                return (caches.match('fallback.html')) //alkup
+              }
+          } else {
+            cache.put(evt.request.url, fetchRes.clone()); //alkup
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 25);
+          }
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1){
+        return caches.match('fallback.html');
+      } 
+    })
+  );
 });
 
+
+self.addEventListener("message", function(event) {
+  event.source.postMessage(event.data + programVersion);
+});
 
