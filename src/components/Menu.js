3a3e5f42d3537435b12c938051bdf33c1d377597
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-07-17 18:06:56 +0300

    Vain versionromuutos

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,7 +8,7 @@ import Info from './Info';
 import { ButtonGroup, GroupLeft, GroupRight } from './Container';
 import helpTexts from '../helpTexts';
 
-const programVersion = '2024-07-17: 159';
+const programVersion = '2024-07-17: 160';
 
 const MenuContainer = styled.div`
   position: fixed;
