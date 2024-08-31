0398b8d610b9e10287784f441da5d5664d15c8a8
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-08-31 15:05:59 +0300

    Vain versionumeron muutos: serviceworker ja menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,7 +11,7 @@ import DeviceInfo from './DeviceInfo';
 import { useSettings } from '../SettingsContext';
 import SwitchButtonComponent from './SwitchButtonCompnent';
 
-const programVersion = '2024-08-31: 1.208';
+const programVersion = '2024-08-31: 1.209';
 
 const MenuContainer = styled.div`
   position: fixed;
