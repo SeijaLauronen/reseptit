121daab06d16dff095ac9e7e990e3964180d5089
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2024-12-22 12:21:21 +0200

    Versionumeron korotus vain, kun ei päivity uusin

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -14,7 +14,7 @@ import ColorManagement from '../ColorManagement';
 import SettingsManagement from '../SettingsManagement';
 import ProductClassManagement from '../ProductClassManagement';
 
-const programVersion = '2024-12-22: 1.251';
+const programVersion = '2024-12-22: 1.252';
 //päivitä versiohistoria myös tänne, huom, vain ostokseni-sovelluksen!:
 //https://github.com/SeijaLauronen/SeijaLauronen.github.io/blob/main/ostokseniversio.html
 //https://seijalauronen.github.io/ostokseniversio.html
