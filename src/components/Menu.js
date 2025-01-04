ca9f84720433b391fc55c9b9d7269992ee702131
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2025-01-04 11:22:20 +0200

    Versionumeron nosto tuoterivin korjaukselle

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -14,7 +14,7 @@ import ColorManagement from '../ColorManagement';
 import SettingsManagement from '../SettingsManagement';
 import ProductClassManagement from '../ProductClassManagement';
 
-const programVersion = '2025-01-04: 2.307';
+const programVersion = '2025-01-04: 2.308';
 //päivitä versiohistoria myös tänne, huom, vain ostokseni-sovelluksen!:
 //https://github.com/SeijaLauronen/SeijaLauronen.github.io/blob/main/ostokseniversio.html
 //https://seijalauronen.github.io/ostokseniversio.html
