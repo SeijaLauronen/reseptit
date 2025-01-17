e19405c995ff313abe583db370c4ca56fc3492d5
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2025-01-17 21:36:07 +0200

    Tutkitaan, onko indexedDB Persistent

--- src/components/StorageInfo.js
+++ src/components/StorageInfo.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import useStorageEstimate from '../hooks/useStorageEstimate';
 
+const formatBytes = (bytes) => {
+    const sizes = ['tavua', 'Kt', 'Mt', 'Gt', 'Tt'];
+    if (bytes === 0) return '0 tavua';
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+};
+
 const StorageInfo = () => {
-    const { usage, quota, percentageUsed, supported } = useStorageEstimate();
+    const { usage, quota, percentageUsed, supported, isPersistent } = useStorageEstimate();
 
     return (
         <div>
             <h1>Tallennustila</h1>
             {supported ? (
                 <div>
-                    <p>Käytetty tallennustila: {usage} tavua</p>
-                    <p>Kiintiö: {quota} tavua</p>
-                    <p>Käytössä oleva osuus: {percentageUsed}%</p>
+                    <p><b>Muista ottaa varmuuskopio, kun olet tallentanut paljon tietoa, joka ei saa hävitä!</b></p>
+                    <p>Tallennustila {isPersistent ? 'on periaatteessa pysyvä.' : 'ei ole pysyvä.'}</p>
+                    <p>Jos poistat selaimen historia- tai selaustietoja, myös tallentamasi tiedot poistetaan.</p>
+                    <p>Selain saattaa myös yllättäin poistaa tietoja muistista tallennustilansa lisäämiseksi,
+                        vaikka alla näkyvä käytössä oleva osuus olisi pienikin. </p>
+                    <p>Käytetty tallennustila: {formatBytes(usage)}<br />
+                    Kiintiö: {formatBytes(quota)}<br/>
+                    Käytössä oleva osuus: {percentageUsed}%<br/>
+                    </p>
                     {percentageUsed > 90 && (
                         <p style={{ color: 'red' }}>Varoitus: Tallennustila lähestyy maksimia! Ota varmuuskopio!</p>
                     )}
@@ -19,7 +32,7 @@ const StorageInfo = () => {
             ) : (
                 <p>Tallennustilan tutkiminen ei ole tuettu tässä selaimessa.</p>
             )}
-           
+
         </div>
     );
 };
--- src/hooks/useStorageEstimate.js
+++ src/hooks/useStorageEstimate.js
@@ -6,22 +6,35 @@ function useStorageEstimate() {
     quota: 0,
     percentageUsed: 0,
     supported: true,
+    isPersistent: false, // Lisätty kenttä pysyvyystarkistukselle
   });
 
   useEffect(() => {
     const fetchStorageEstimate = async () => {
+      console.log('fetchStorageEstimate');
       if ('storage' in navigator && 'estimate' in navigator.storage) {
         try {
           const estimate = await navigator.storage.estimate();
           const usage = estimate.usage || 0;
           const quota = estimate.quota || 0;
           const percentageUsed = quota > 0 ? (usage / quota) * 100 : 0;
+          // Tarkistetaan onko tallennustila pysyvä
+          const isPersistent = await navigator.storage.persisted();
+          console.log('Pysyvyys?:', isPersistent);
+          // Pyydetään pysyvyys automaattisesti, jos sitä ei ole
+          /*
+          if (!isPersistent) {
+            const isGranted = await navigator.storage.persist();
+            console.log('Pysyvyys pyydetty:', isGranted);
+          } 
+          */         
 
           setStorageInfo({
             usage,
             quota,
             percentageUsed: percentageUsed.toFixed(2),
             supported: true,
+            isPersistent, // Päivitetään pysyvyyden tila
           });
         } catch (error) {
           console.error("Storage estimate API ei toiminut:", error);
