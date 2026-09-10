c5499ce87500215433ffdf586c4e26f746371c02
SeijaLauronen <60699051+SeijaLauronen@users.noreply.github.com>
2026-09-10 22:08:15 +0300

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
-const programVersion = '2026-09-10: 2.366';
-const staticCacheName = 'recipe-static-2366'; 
-const dynamicCacheName = 'recipe-dynamic-2366'; // Ei välttämätön
+const programVersion = '2026-09-10: 2.367';
+const staticCacheName = 'recipe-static-2367'; 
+const dynamicCacheName = 'recipe-dynamic-2367'; // Ei välttämätön
 
 const assets = [
   '/',
